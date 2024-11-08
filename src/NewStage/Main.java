e886eed7ab5c247d74452971d71f7180db3188fb
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-08 23:12:05 +0600

    configaration Updated

--- src/NewStage/Main.java
+++ src/NewStage/Main.java
@@ -5,11 +5,8 @@
 package NewStage;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
