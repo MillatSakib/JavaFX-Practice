4bd4d728c1f50a66f692731a8f367031d9320ee6
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-08 11:21:04 +0600

    Completed

---
+++ src/Controller/Main.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent pane = FXMLLoader.load(getClass().getResource("/View/FXML.fxml"));
+            Scene scene = new Scene(pane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace(); // Print the exception to identify any issues
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
