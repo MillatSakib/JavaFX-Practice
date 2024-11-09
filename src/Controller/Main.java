3097031d2125f8249975a64f177b6f4f9d6e292f
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-09 21:39:35 +0600

    basic code completed

---
+++ src/Controller/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Sakib
+ */
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/MultipleScene.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Multiple Scene");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+    launch(args);
+    }
+}
